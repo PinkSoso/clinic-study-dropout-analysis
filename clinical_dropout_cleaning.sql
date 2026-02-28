9b6f7b50afe4be7bfae16bf9dd4e4c747c352724
Sonia M <sonia.merzoud@hotmail.fr>
2026-02-28 12:18:22 +0100

    Update clinical_dropout_cleaning.sql

--- clinical_dropout_cleaning.sql
+++ clinical_dropout_cleaning.sql
@@ -1,6 +1,17 @@
 -- PARTIE 1 : NETTOYAGE DES DATAS / DATA CLEANING --
 
--- Table 'Appointment'
+-- 'Appointment' Table--
+--View Creation--
+CREATE VIEW mis602_ass2.appointment_clean AS
+SELECT
+appointment_id,
+patient_id,
+doctor_id,
+appointment_date,
+notes,
+status,
+FROM mis602_ass2.appointment;
+
 -- Identifier les NULLS/ Valeurs Manquantes/ Chaînes Vides/ Espace (OK)
 SELECT
 COUNT(*) AS Total_rows,
@@ -10,7 +21,15 @@ SUM(CASE WHEN appointment_id IS NULL THEN 1 ELSE 0 END) AS null_appointment_id,
   SUM(CASE WHEN appointment_date IS NULL THEN 1 ELSE 0 END) AS null_appointment_date,
   SUM(CASE WHEN notes IS NULL THEN 1 ELSE 0 END) AS null_notes,
   SUM(CASE WHEN status IS NULL THEN 1 ELSE 0 END) AS null_status
-  FROM appointment_clean;
+  FROM mis602_ass2.appointment_clean;
+
+-- Compter le nombre de ligne avec Espace ou des valeurs NULL dans la table 'appointment' colonne 'status'
+SELECT
+COUNT(*) AS total_rows,
+SUM(CASE 
+WHEN status IS NOT NULL AND (status = '' OR NULLIF(TRIM(status), '') IS NULL) THEN 1 ELSE 0 END) AS empty_only,
+SUM(CASE WHEN status IS NULL THEN 1 ELSE 0 END) AS Null_Value
+FROM mis602_ass2.appointment_clean;
 
 -- Remplacer les valeurs NULLS de la table 'appointment' (OK)
 SELECT
@@ -23,7 +42,28 @@ COALESCE(patient_id, 0) AS clean_patient_id,
 COALESCE(appointment_id, 0) AS clean_appointment_id
 FROM mis602_ass2.appointment_clean;
 
--- Table Doctor --
+-- Identifier dans la PK (appointment_id) de la table 'appointment' des doublons--
+SELECT
+*
+FROM
+(SELECT
+appointment_id,
+COUNT(*) OVER (PARTITION BY appointment_id) AS CheckPK
+FROM mis602_ass2.appointment_clean
+) t 
+WHERE CheckPK > 1;
+
+
+-- 'Doctor' table --
+--Create a view--
+CREATE VIEW mis602_ass2.doctor_clean AS
+SELECT
+doctor_id,
+name,
+phone_number,
+speciality_id,
+FROM mis602_ass2.doctor;
+
 -- Identifier les Valeurs Nulles + Remplacer les valeurs NULLS de la table 'doctor' version CTE
 WITH clean_doctor_report AS (
 SELECT
@@ -52,122 +92,6 @@ SELECT
   END AS clean_speciality_id
   FROM doctor_clean;
 
--- Table medication (ok aucun NULL trouvé)
-SELECT
-COUNT(*) AS Total_rows,
-SUM(CASE WHEN medication_id IS NULL THEN 1 ELSE 0 END) AS null_medication_id,
-  SUM(CASE WHEN name IS NULL THEN 1 ELSE 0 END) AS null_name,
-  SUM(CASE WHEN manufacturer IS NULL THEN 1 ELSE 0 END) AS null_manufacturer,
-  SUM(CASE WHEN dosage_form IS NULL THEN 1 ELSE 0 END) AS null_dosage_form,
-  SUM(CASE WHEN strength IS NULL THEN 1 ELSE 0 END) AS null_strength,
-  SUM(CASE WHEN description IS NULL THEN 1 ELSE 0 END) AS null_description
-  FROM mis602_ass2.medication_clean;
-
--- Table patient
-WITH clean_patient AS (
-SELECT
-COUNT(*) AS Total_rows,
-SUM(CASE WHEN patient_id IS NULL THEN 1 ELSE 0 END) AS null_patient_id;
-FROM mis602_ass2.doctor_clean
-)
-SELECT 
-*
-FROM patient
-WHERE patient_id IS NULL
-AND name IS NULL
-AND dob IS NULL
-AND gender IS NULL
-AND address IS NULL
-AND state_code IS NULL;
-FROM mis602_ass2.patient_id_clean;
-
--- Table prescription (ok aucun NULL trouvé)
-SELECT *
-FROM prescription
-WHERE prescription_id IS NULL
-AND appointment_id IS NULL
-AND medication_id IS NULL;
-
--- Table speciality (ok aucun NULL trouvé)
-SELECT *
-FROM speciality
-WHERE speciality_id IS NULL
-AND name IS NULL;
-
--- Compter le nombre de ligne avec Espace ou des valeurs NULL dans la table 'appointment' colonne 'status' (RAS)
-SELECT
-COUNT(*) AS total_rows,
-SUM(CASE 
-WHEN status IS NOT NULL AND (status = '' OR NULLIF(TRIM(status), '') IS NULL) THEN 1 ELSE 0 END) AS empty_only,
-SUM(CASE WHEN status IS NULL THEN 1 ELSE 0 END) AS Null_Value
-FROM appointment_clean;
-
--- Création d'une copie de la table pour une table plus clean, modifiable et d'ajout de colonne (OK)
-CREATE VIEW mis602_ass2.appointment_clean AS
-SELECT
-appointment_id,
-patient_id,
-doctor_id,
-appointment_date,
-notes,
-status,
-COALESCE(status, 'unknown') AS clean_status
-FROM mis602_ass2.appointment_clean;
-
-
-CREATE VIEW mis602_ass2.medication_clean AS
-SELECT
-medication_id,
-name,
-manufacturer,
-dosage_form,
-strength,
-description
-FROM mis602_ass2.medication;
-
-CREATE VIEW mis602_ass2.patient_id_clean AS
-SELECT
-patient_id,
-name,
-dob,
-gender,
-phone_number,
-address,
-state_code
-FROM mis602_ass2.patient;
-
-CREATE VIEW mis602_ass2.prescription_clean AS
-SELECT
-prescription_id,
-appointment_id,
-medication_id
-FROM mis602_ass2.prescription;
-
-CREATE VIEW mis602_ass2.speciality_clean AS
-SELECT
-speciality_id,
-name
-FROM mis602_ass2.speciality;
-
--- Identifier dans la PK (appointment_id) de la table 'appointment' des doublons (ok no duplicates)
-SELECT
-*
-FROM
-(SELECT
-appointment_id,
-COUNT(*) OVER (PARTITION BY appointment_id) AS CheckPK
-FROM mis602_ass2.appointment_clean
-) t 
-WHERE CheckPK > 1;
-
--- Identifier si doublon de PK (speciality_id) de la table 'speciality' (s)
-SELECT 
-speciality_id,
-COUNT(speciality_id) AS Duplicates
-FROM speciality
-GROUP BY speciality_id
-HAVING COUNT(speciality_id) > 1;
-
 -- Identifier les doublons de PK (doctor_id) de la table 'doctor' (ok no duplicates)
 SELECT
 *
@@ -178,6 +102,30 @@ COUNT(*) OVER (PARTITION BY doctor_id) AS CheckPK
 FROM doctor_clean
 ) t
 WHERE CheckPK > 1;
+
+
+-- Table 'medication' --
+-- Create view --
+CREATE VIEW mis602_ass2.medication_clean AS
+SELECT
+medication_id,
+name,
+manufacturer,
+dosage_form,
+strength,
+description
+FROM mis602_ass2.medication;
+
+-- Identifier les Valeurs Nulles + Remplacer les valeurs NULLS de la table 'medication' version CTE --
+SELECT
+COUNT(*) AS Total_rows,
+SUM(CASE WHEN medication_id IS NULL THEN 1 ELSE 0 END) AS null_medication_id,
+  SUM(CASE WHEN name IS NULL THEN 1 ELSE 0 END) AS null_name,
+  SUM(CASE WHEN manufacturer IS NULL THEN 1 ELSE 0 END) AS null_manufacturer,
+  SUM(CASE WHEN dosage_form IS NULL THEN 1 ELSE 0 END) AS null_dosage_form,
+  SUM(CASE WHEN strength IS NULL THEN 1 ELSE 0 END) AS null_strength,
+  SUM(CASE WHEN description IS NULL THEN 1 ELSE 0 END) AS null_description
+  FROM mis602_ass2.medication_clean;
 
 -- Identifier les doublon de PK (medication_id) de la table 'medication' (ok no duplicates)
 SELECT
@@ -190,6 +138,38 @@ FROM medication_clean
 ) t
 WHERE CheckPK > 1;
 
+
+
+-- Table patient--
+-- Creat view table--
+CREATE VIEW mis602_ass2.patient_id_clean AS
+SELECT
+patient_id,
+name,
+dob,
+gender,
+phone_number,
+address,
+state_code
+FROM mis602_ass2.patient;
+
+-- Identifier les Valeurs Nulles + Remplacer les valeurs NULLS de la table 'patient' version CTE (ok no Null values)
+WITH clean_patient AS (
+SELECT
+COUNT(*) AS Total_rows,
+SUM(CASE WHEN patient_id IS NULL THEN 1 ELSE 0 END) AS null_patient_id
+FROM mis602_ass2.doctor_clean
+)
+SELECT 
+*
+FROM patient
+WHERE patient_id IS NULL
+AND name IS NULL
+AND dob IS NULL
+AND gender IS NULL
+AND address IS NULL
+AND state_code IS NULL
+
 -- Identifier les doublons de PK (patient_id) de la table 'patient' (ok no duplicates)
 SELECT
 *
@@ -201,6 +181,23 @@ FROM patient_id_clean
 ) t
 WHERE CheckPK > 1;
 
+
+--'Prescription' Table--
+--Create view
+CREATE VIEW mis602_ass2.prescription_clean AS
+SELECT
+prescription_id,
+appointment_id,
+medication_id
+FROM mis602_ass2.prescription;
+
+-- Identifier les Valeurs Nulles + Remplacer les valeurs NULLS de la table 'prescription' (ok no null values)
+SELECT *
+FROM mis602_ass2.prescription_clean
+WHERE prescription_id IS NULL
+AND appointment_id IS NULL
+AND medication_id IS NULL;
+
 -- Identifier les doublons de PK (prescription_id) de la table 'prescription' (ok no duplicates)
 SELECT
 *
@@ -208,93 +205,28 @@ FROM (
 SELECT
 prescription_id,
 COUNT(*) OVER (PARTITION BY prescription_id) AS CheckPK
-FROM prescription_clean
+FROM mis602_ass2.prescription_clean
 ) t
 WHERE CheckPK > 1;
 
--- Remplacer les valeurs NULLS de la table 'appointment' concernant la colonne 'status' (OK)
+--Table 'speciality'--
+--Create View--
+CREATE VIEW mis602_ass2.speciality_clean AS
 SELECT
-status,
-COALESCE(status, 'unknown') AS clean_status
-FROM mis602_ass2.appointment_clean;
+speciality_id,
+name
+FROM mis602_ass2.speciality;
 
--- PARTIE 2 : ANALYSE DES DATAS --
+-- Table speciality (ok no NULL values)
+SELECT *
+FROM mis602_ass2.speciality_clean
+WHERE speciality_id IS NULL
+AND name IS NULL;
 
--- Compter le nombre unique de patients, de docteurs et de status (OK)
+-- Identifier si doublon de PK (speciality_id) de la table 'speciality'
 SELECT 
-  COUNT(DISTINCT patient_id) AS unique_patients,
-  COUNT(DISTINCT doctor_id) AS unique_doctors,
-  COUNT(DISTINCT status) AS unique_status,
-  COUNT(*) AS total_appointments
-FROM appointment_clean;
-
--- Compter le total des 'completed' en clean_status (KO) 
-SELECT
-clean_status,
-patient_id,
-COUNT(DISTINCT(clean_status)) OVER (PARTITION BY patient_id) AS total_status
-FROM mis602_ass2.appointment_clean
-WHERE clean_status = 'completed';
-
--- Decompter le nombre de clean_status 'unkown', 'completed' et 'cancelled' par patient (OK)
-WITH clean_status_total AS
-(
-SELECT
-clean_status,
-count(*) AS Total_status
-FROM appointment_clean
-GROUP BY clean_status
-ORDER BY clean_status DESC
-)
-SELECT
-patient_id,
-COUNT(*) OVER (PARTITION BY patient_id) AS Total_status_per_client
-FROM appointment_clean
-ORDER BY Patient_id DESC;
-
-SELECT
-patient_id,
-clean_status,
-COUNT(*) OVER (PARTITION BY patient_id) AS total_per_patient
-FROM appointment_clean
-GROUP by patient_id, clean_status
-ORDER BY patient_id ASC;
-
--- Isoler les patients qui ont des doublons de status (OK)
-WITH patient_clean_status_duplicates AS
-(
-SELECT
-patient_id,
-clean_status,
-COUNT(*) OVER (PARTITION BY patient_id) AS total_per_patient
-FROM appointment_clean
-GROUP by patient_id, clean_status
-ORDER BY patient_id ASC
-)
-SELECT 
-patient_id,
-clean_status,
-total_per_patient
-FROM patient_clean_status_duplicates
-WHERE total_per_patient >= 2
-ORDER BY patient_id;
--- on peut voir que 5 patient on des doublons de status ce qui va interefere dans nos résultats 
-
--- Comptabiliser le nombre d'appointment_id par patient (OK)
-WITH patient_appointments_duplicates AS
-(
-SELECT
-patient_id,
-appointment_id,
-COUNT(*) OVER (PARTITION BY patient_id) AS total_appointment_per_patient
-FROM appointment_clean
-GROUP by patient_id, appointment_id
-ORDER BY patient_id ASC
-)
-SELECT 
-patient_id,
-appointment_id,
-total_appointment_per_patient
-FROM patient_appointments_duplicates
-WHERE total_appointment_per_patient >= 2
-ORDER BY patient_id;
+speciality_id,
+COUNT(speciality_id) AS Duplicates
+FROM mis602_ass2.speciality_clean
+GROUP BY speciality_id
+HAVING COUNT(speciality_id) > 1;
